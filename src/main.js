2c865ff948b2f9cbdda65b58f08bd1cbd4cd3eb8
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-23 23:45:36 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import GitGuide from './components/GitGuide.vue'
 import Repos from './components/Repos.vue'
 import Contribute from './components/Contribute.vue'
 import Docs from './components/Docs.vue'
+import Signing from './components/Signing.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/git', component: GitGuide },
   { path: '/repos', component: Repos },
   { path: '/contribute', component: Contribute },
-  { path: '/docs', component: Docs }
+  { path: '/docs', component: Docs },
+  { path: '/docs/signing', component: Signing }
 ]
 
 const router = createRouter({
